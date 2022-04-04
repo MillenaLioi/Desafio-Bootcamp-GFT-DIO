65129d83ff128308cdc139fca6d2daaf26ca8f07
Millenalioi <millena.lioi@gmail.com>
2022-04-03 21:55:22 -0300

    Repositorio

---
+++ Conta Corrente.java
@@ -0,0 +1,9 @@
+
+public class ContaCorrente extends Conta {
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.printin("=== Extrato Conta Corrente ===");
+        super.imprimirInfosComuns
+    }
+}
---
+++ Conta Poupança.java
@@ -0,0 +1,9 @@
+
+public class ContaPoupanca extends Conta {
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.printin("=== Extrato Conta Poupanca ===");
+        super.imprimirInfosComuns
+    }
+}
